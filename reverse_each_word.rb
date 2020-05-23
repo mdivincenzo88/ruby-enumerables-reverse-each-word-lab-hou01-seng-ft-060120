04824eb60c435c1d4026cd0e911faabbead221ed
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-23 15:39:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
 def reverse_each_word(string)
   my_array= []
   my_array = string.split(" ")
+<<<<<<< HEAD
    my_array.collect {|word| word.reverse}.join(" ")
   end
+=======
+   my_array.collect {|word| word.reverse}.join()
+   
+ end
+>>>>>>> 4035b32d45413313b69b1421f00b3f5fbc83d339
 
 
 
